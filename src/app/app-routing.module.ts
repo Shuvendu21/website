297c43fccfc955971f83886502d4b242bf4b5315
Shuvendu21 <shuvendu0000@gmail.com>
2021-04-16 09:29:26 +0530

    first commit160421

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutModule } from './about/about.module';
+import { ContactusComponent } from './contactus/contactus.component';
+import { HomeModule } from './home/home.module';
+import { LoginComponent } from './login/login.component';
+import { NotfoundModule } from './notfound/notfound.module';
+import { SignupComponent } from './signup/signup.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => HomeModule },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'contactus', component: ContactusComponent, outlet: 'popup'  },
+  { path: 'about', loadChildren: () => AboutModule },
+  // { path: 'services', loadChildren: () => ServicesModule },
+  // { path: 'testimonials', loadChildren: () => TestimonialModule },
+  // { path: 'gallery', loadChildren: () => GalleryModule },
+  // { path: 'clients', loadChildren: () => ClientsModule },
+  // { path: 'pricing', loadChildren: () => PricingModule },
+  // { path: 'subscribe', component: SubscribeComponent, outlet: 'popup' },
+  // { path: 'dashboard', loadChildren: () => UserDashboardModule, canActivate: [RoutegaurdService]   },
+  // { path: 'blog', loadChildren: () => BlogModule, canActivate: [RoutegaurdService] },
+  { path: '404', loadChildren: () => NotfoundModule },
+  { path: '**', redirectTo: '/404' },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,47 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { NavmenuComponent } from './navmenu/navmenu.component';
+import { SignupComponent } from './signup/signup.component';
+import { SocialComponent } from './social/social.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HomeModule } from './home/home.module';
+import { AboutModule } from './about/about.module';
+import { NotfoundModule } from './notfound/notfound.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavigationComponent,
+    NavmenuComponent,
+    SignupComponent,
+    SocialComponent,
+    FooterComponent,
+    ContactusComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    ReactiveFormsModule,
+    // HttpClientModule,
+    HomeModule,
+    AboutModule,
+    // ServicesModule,
+    // GalleryModule,
+    // UserDashboardModule,
+    NotfoundModule,
+    // ClientsModule,
+    // TestimonialModule,
+    // PricingModule,
+    // BlogModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
